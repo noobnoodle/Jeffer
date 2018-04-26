4b8f3780b6292c9b82f4be537332c5b9b3fdbbdf
noobnoodle <38495431+noobnoodle@users.noreply.github.com>
2018-04-26 20:42:19 +0800

    1

--- js/changelist.js
+++ js/changelist.js
@@ -3,6 +3,7 @@ function changelist(){
     var $l2 = $('#p3 ul .l2');
     $l1.click(function(){
         var lindex = 0.5*$(this).index();
-        $l2.eq(lindex).stop().animate({height:'75.5%'},1200).siblings('.l2').animate({height:'0'},1);
+        if($l2.eq(lindex).height() != 0){$l2.eq(lindex).stop().animate({height:'0'},1200);}
+        else{$l2.eq(lindex).stop().animate({height:'75.5%'},1200).siblings('.l2').animate({height:'0'},1189);}
     })
 }
