48f09a4657a0cc32e88abff77208f5a5aef76fa2
noobnoodle <38495431+noobnoodle@users.noreply.github.com>
2018-04-26 23:08:21 +0800

    1

--- js/changelist.js
+++ js/changelist.js
@@ -2,8 +2,10 @@ function changelist(){
     var $l1 = $('#p3 ul .l1');
     var $l2 = $('#p3 ul .l2');
     $l1.click(function(){
-        var lindex = 0.5*$(this).index();
-        if($l2.eq(lindex).height() != 0){$l2.eq(lindex).stop().animate({height:'0'},1200);}
-        else{$l2.eq(lindex).stop().animate({height:'75.5%'},1200).siblings('.l2').animate({height:'0'},1189);}
+        var lindex = $(this).index();
+        $l1.eq(lindex).css('border','3px groove rgba(0,0,0,0.35)').siblings('.l1').css('border','1px solid rgba(0,0,0,0.15)');
+        $l1.eq(lindex).css('background-color','rgba(0,0,0,0.35)').siblings('.l1').css('background-color','rgba(0,0,0,0.15)');
+        $l1.eq(lindex).css('border-right-width','0px').siblings('.l1').css('border-right','1px solid rgba(0,0,0,0.15)');
+        $l2.eq(lindex).fadeIn(500).siblings('.l2').fadeOut(500);
     })
 }
